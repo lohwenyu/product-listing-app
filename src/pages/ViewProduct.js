708aa2b21ad8ce2313c513394c4b94c9c5238564
Loh Wen Yu <lohwenyu@Lohs-MacBook-Pro-2.local>
2023-04-27 15:35:53 +0800

    detailed product view

--- src/pages/ViewProduct.js
+++ src/pages/ViewProduct.js
@@ -1,16 +1,49 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Image, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+
+import { useHttpClient } from "../hooks/http-hook";
+
+import './ViewProduct.css';
 
 const ViewProduct = () => {
-    const params = useParams();
-    const uid = params.productUid;
 
-    
+    const { productUid } = useParams();
+    const navigate = useNavigate();
+
+    const [loadedProduct, setLoadedProduct] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const responseData = await sendRequest(`http://localhost:8080/api/listings/id/${productUid}`);
+                setLoadedProduct(responseData.listing[0]);
+            } catch (err) { };
+        };
+        fetchProduct();
+        console.log(loadedProduct)
+    }, [sendRequest]);
+
     return (
-        <div>
-            ViewProductPage: {uid}
+        <div className="main-container">
+            <h1>Product Details</h1>
+            {loadedProduct &&
+                <div className="product-details-container">
+                    <Image src={`http://localhost:8080/${loadedProduct.image}`} className="large-product-image" />
+                    <div className="product-details">
+                        <h3>
+                            {loadedProduct.name}
+                        </h3>
+                        <h4>${loadedProduct.price}</h4>
+                        <Button variant="outline-secondary" className="mb-3" disabled size="sm">{loadedProduct.category}</Button>
+                        <p>{loadedProduct.description}</p>
+                    </div>
+                </div>
+            }
         </div>
-    );
+    )
+
 };
 
 export default ViewProduct;
